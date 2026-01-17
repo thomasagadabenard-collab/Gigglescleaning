0869f7adbc0573cda6e0a3b43a5b11230e4b69a2
Thomas Benard <thomasagadabenard@gmail.com>
2026-01-17 20:37:40 +0100

    animation correction

--- script.js
+++ script.js
@@ -27,47 +27,4 @@ window.addEventListener("touchstart", closeMenus);
 
 
 
-const masterTl = gsap.timeline();
-
-masterTl.from(".logo", {
-  duration: 1,
-  opacity:0,
-  ease: "power1.out"
-});
-
-
-masterTl.fromTo(".nav-lists li",
-  {opacity: 0, y: -20 },   
-  {opacity:1, y: 0, duration: 1, ease: "power1.out", stagger: 0.2 } 
-);
-
-
-masterTl.from(".hamburger", {
-  duration: 1,
-  opacity: 0,
-  ease: "power1.out"
-}, "-=0.8"); 
-
-
-masterTl.from(".hero-text span, .hero-text h1", {
-  duration: 1.5,
-  opacity: 0,
-  ease: "power2.out",
-  stagger: 0.1
-}, "-=0.5"); 
-
-// Animate hero buttons
-masterTl.from(".hero-buttons", {
-  duration: 1.5,
-  opacity: 0,
-  ease: "bounce.out",
-  stagger: 0.3
-}, "-=1"); 
-
-masterTl.from(".hero-images", {
-  duration: 1,
-  opacity: 0,
-  ease: "power2.out",
-  stagger: 0.2
-}, "-=1"); 
 
