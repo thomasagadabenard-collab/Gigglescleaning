ac439a8032db4fb2a111e4172ac79ef3a16e7b9b
Thomas Benard <thomasagadabenard@gmail.com>
2026-04-12 22:01:04 +0100

    first commit today

--- script.js
+++ script.js
@@ -3,28 +3,41 @@ const hamburger = document.querySelector(".hamburger");
 const servicesDropdown = document.querySelector(".services-dropdown");
 const dropDown = document.querySelector(".dropdown-menu");
 
+let overlay;
 
-hamburger.addEventListener("click", (e) => {
-  e.stopPropagation(); 
-  navList.classList.toggle("active");
-});
+const createOverlay = () => {
+  if (!overlay) {
+    overlay = document.createElement("div");
+    overlay.className = "overlay";
+    document.body.appendChild(overlay);
 
-servicesDropdown.addEventListener("click", (e) => {
-  e.preventDefault();
-  e.stopPropagation(); 
-  dropDown.classList.toggle("active");
-  servicesDropdown.classList.toggle("active");
-});
+    overlay.addEventListener("click", closeMenus);
+    overlay.addEventListener("touchstart", closeMenus);
+  }
+};
+
+const removeOverlay = () => {
+  if (overlay) {
+    overlay.remove();
+    overlay = null;
+  }
+};
 
 const closeMenus = () => {
   navList.classList.remove("active");
   dropDown.classList.remove("active");
   servicesDropdown.classList.remove("active");
+  removeOverlay();
 };
 
-window.addEventListener("click", closeMenus);
-window.addEventListener("touchstart", closeMenus); 
+hamburger.addEventListener("click", (e) => {
+  e.stopPropagation();
+  navList.classList.toggle("active");
+});
 
-
-
-
+servicesDropdown.addEventListener("click", (e) => {
+  e.stopPropagation();
+  createOverlay();
+  dropDown.classList.toggle("active");
+  servicesDropdown.classList.toggle("active");
+});
